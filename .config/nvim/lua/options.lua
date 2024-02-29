0a53faa5432e7641cce00b1d6d02974619404835
William Fuller <williamfulle@gmail.com>
2024-02-29 23:44:24 +0100

    added a bunch of options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -5,8 +5,14 @@ vim.cmd('highlight SignColumn ctermfg=NONE ctermbg=NONE')
 vim.opt.clipboard = 'unnamedplus'   -- use system clipboard 
 vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
 
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+
 -- UI config
 vim.opt.number = true               -- show absolute number
+vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes'
 vim.opt.laststatus = 2
 vim.opt.showmode = false
@@ -14,4 +20,4 @@ vim.opt.showmode = false
 -- Searching
 vim.opt.incsearch = true            -- search as characters are entered
 vim.opt.ignorecase = true           -- ignore case in searches by default
-vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
+vim.opt.smartcase = true           -- but make it case sensitive if an uppercase is entered
