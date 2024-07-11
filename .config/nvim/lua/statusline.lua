bbbc4b765f8604400d1942ae6a043eb783e658e3
William Fuller <williamfulle@gmail.com>
2024-07-11 13:51:07 +0200

    removed warning/error from statusline bad not used

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -26,47 +26,12 @@ local function mode(current_mode)
 	return string.format(" %s ", modes[current_mode]):upper()
 end
 
-local function lsp()
-  local count = {}
-  local levels = {
-    errors = "Error",
-    warnings = "Warn",
-    info = "Info",
-    hints = "Hint",
-  }
-
-  for k, level in pairs(levels) do
-    count[k] = vim.tbl_count(vim.diagnostic.get(0, { severity = level }))
-  end
-
-  local errors = ""
-  local warnings = ""
-  local hints = ""
-  local info = ""
-
-  if count["errors"] ~= 0 then
-    errors = "×" .. count["errors"]
-  end
-  if count["warnings"] ~= 0 then
-    warnings = "!" .. count["warnings"]
-  end
-  if count["hints"] ~= 0 then
-    hints = "i" .. count["hints"]
-  end
-  if count["info"] ~= 0 then
-    info = "i" .. count["info"]
-  end
-
-  return errors .. " " .. warnings .. " " .. hints .. " " .. info 
-end
-
 vim.api.nvim_create_autocmd({"BufEnter", "WinEnter","ModeChanged"}, {
   	pattern = "*",
 	callback = function()
-		vim.opt.statusline = string.format("%s %s %s%s%s",
+		vim.opt.statusline = string.format("%s %s%s%s",
 			mode(),
 			"%f",
-			lsp(),
 			"%=",
 			" %P %l:%c ")
   end
