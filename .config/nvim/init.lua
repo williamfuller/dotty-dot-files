28950a53d952aee39cd9c541d407c6eb19f8e0bf
William Fuller <william@superg.fr>
2024-11-15 12:18:56 +0100

    colorscheme better

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = ','
-vim.cmd('colorscheme slate')
 
 require('options')
 require('plugins')
@@ -9,16 +8,4 @@ require('fexplorer')
 require('statusline')
 
 -- colortheme
-local theme = require('tokyonight')
-
-theme.setup({
-	style = 'storm',
-})
-theme.load()
-
-if vim.g.neovide then
- 	vim.g.neovide_cursor_animation_length = 0
-  vim.g.neovide_scroll_animation_length = 0.07
-  vim.g.neovide_background_color = "#0E0E27" 	
-	vim.o.guifont = "Cascadia Code:h10"
-end
+vim.cmd('colorscheme tokyonight-night')
